6208b35207697bb2c72296d8852e0965bc55576d
Radhika <radhika.bhatt@kiprosh.com>
2013-08-29 21:23:40 +0530

    First assignment second file

--- Author.rb
+++ Author.rb
@@ -4,25 +4,26 @@ require 'mysql'
 module Authors
 
 class Author
-attr_accessor :authorArray, :temp
-@authorHash= Hash.new
-@authorArray= Array.new
+	attr_accessor :authorArray, :temp
+	@authorHash= Hash.new
+	@authorArray= Array.new
 
-$db= Mysql.connect('localhost','root','password','test')
-def initialize
-@authorArray= []
-# authors would be Serialized array of unique_ids from author table
-query=$db.query("SELECT * FROM authors")
-query.each_hash do |h|
-@authorHash = h
-#validation on authors
-@temp= @authorHash['name'].scan(/[^a-zA-Z0-9]/)
-raise InvalidAuthorName, "Enter valid author name" if @authorHash['name']== "" && temp.count !=0 
-@authorArray << @authorHash
-@authorArray
-end
-end
-end
+	$db= Mysql.connect('localhost','root','password','test')
+		def initialize
+		@authorArray= []
+		# authors would be Serialized array of unique_ids from author table
+		query=$db.query("SELECT * FROM authors")
+			query.each_hash do |h|
+			@authorHash = h
+			
+			#validation on authors
+			@temp= @authorHash['name'].scan(/[^a-zA-Z0-9]/)
+			raise InvalidAuthorName, "Enter valid author name" if @authorHash['name']== "" && temp.count !=0 
+			@authorArray << @authorHash
+			@authorArray
+			end
+		end
+	end
 end
 
 
